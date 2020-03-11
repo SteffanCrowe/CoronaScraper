a62d1b87299af6c6414c75c115976daaa00dc15c
Steffan Crowe <fub4r@hotmail.co.uk>
2020-03-11 13:55:33 +0000

    Add fail safe for when table cannot be found

--- Scraper.py
+++ Scraper.py
@@ -12,34 +12,38 @@ class Scraper:
         page = requests.get(self.URL)
         soup = BeautifulSoup(page.content, 'html.parser')
         table = soup.find(id='main_table_countries')
-        table_body = table.find('tbody')
+        table_body = None
+        if table is not None:
+            table_body = table.find('tbody')
         return table_body
 
     def GetNumInfectedFromTable(self, table):
-        rows = table.find_all('tr')
-        for row in rows:
-            cols = row.find_all('td')
-            countryName = cols[0].find(text=True)
+        if table is not None:
+            rows = table.find_all('tr')
+            for row in rows:
+                cols = row.find_all('td')
+                countryName = cols[0].find(text=True)
 
-            if countryName.startswith(' UK'):
-                numInfected = cols[1].find(text=True)
-                return numInfected
-        return 0
+                if countryName.startswith(' UK'):
+                    numInfected = cols[1].find(text=True)
+                    return numInfected
+        return None
 
     def GetNumInfected(self):
         table = self.GetTable()
         return self.GetNumInfectedFromTable(table)
 
 def printNumInfected(scraper):
-  threading.Timer(60.0, printNumInfected, [scraper]).start()
-  global numInfected
-  currentNumInfected = scraper.GetNumInfected()
-  if numInfected != currentNumInfected:
-    numInfected = currentNumInfected
-    currentTime = time.gmtime()
-    formattedTime = time.strftime("%c", currentTime)
-    print("{} : {} Infected".format(formattedTime, numInfected))
-    ctypes.windll.user32.MessageBoxW(0, "The current number of infected has increased to {}".format(numInfected), "U.K Corona", 0)
+    threading.Timer(60.0, printNumInfected, [scraper]).start()
+    global numInfected
+    currentNumInfected = scraper.GetNumInfected()
+    if currentNumInfected is not None:
+        if numInfected != currentNumInfected:
+            numInfected = currentNumInfected
+            currentTime = time.gmtime()
+            formattedTime = time.strftime("%c", currentTime)
+            print("{} : {} Infected".format(formattedTime, numInfected))
+            ctypes.windll.user32.MessageBoxW(0, "The current number of infected has increased to {}".format(numInfected), "U.K Corona", 0)
 
 numInfected = 0
 scraper = Scraper()
