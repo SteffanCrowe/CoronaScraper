95401f60b14cb775d6ffb9837f85d1c5aa6ef310
Steffan Crowe <fub4r@hotmail.co.uk>
2020-03-11 16:06:39 +0000

    Fix issue parsing UK row now it's been hyperlinked

--- Scraper.py
+++ Scraper.py
@@ -9,24 +9,29 @@ class Scraper:
         self.URL = 'https://www.worldometers.info/coronavirus/'
 
     def GetTable(self):
-        page = requests.get(self.URL)
-        soup = BeautifulSoup(page.content, 'html.parser')
-        table = soup.find(id='main_table_countries')
-        table_body = None
-        if table is not None:
-            table_body = table.find('tbody')
-        return table_body
+        try:
+            page = requests.get(self.URL)
+            soup = BeautifulSoup(page.content, 'html.parser')
+            table = soup.find(id="main_table_countries")
+            table_body = None
+            if table is not None:
+                table_body = table.find('tbody')
+            return table_body
+        except:
+            return None # Problems requesting the page? just return none
 
     def GetNumInfectedFromTable(self, table):
         if table is not None:
             rows = table.find_all('tr')
             for row in rows:
                 cols = row.find_all('td')
-                countryName = cols[0].find(text=True)
-
-                if countryName.startswith(' UK'):
-                    numInfected = cols[1].find(text=True)
-                    return numInfected
+                countryNameColumn = cols[0]
+                countryNameHyper = countryNameColumn.find('a')
+                if countryNameHyper is not None:
+                    countryName = countryNameHyper.find(text=True)
+                    if countryName.startswith('UK'):
+                        numInfected = cols[1].find(text=True)
+                        return numInfected
         return None
 
     def GetNumInfected(self):
@@ -44,6 +49,8 @@ def printNumInfected(scraper):
             formattedTime = time.strftime("%c", currentTime)
             print("{} : {} Infected".format(formattedTime, numInfected))
             ctypes.windll.user32.MessageBoxW(0, "The current number of infected has increased to {}".format(numInfected), "U.K Corona", 0)
+    else:
+        print("Error getting table from " + scraper.URL)
 
 numInfected = 0
 scraper = Scraper()
